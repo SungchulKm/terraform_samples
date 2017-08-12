88af7ab4f9298aa80644c846d44c6bdfaac0367b
schulkim <schulkim@naver.com>
2017-08-12 19:40:12 +0900

    autoscaling

--- main.tf
+++ main.tf
@@ -11,8 +11,26 @@ resource "aws_instance" "example" {
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+  lifecycle {
+    create_before_destroy = true
+  }
   tags {
     Name = "terraform-example"
+  }
+  key_name = "aws_key"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -23,5 +41,14 @@ resource "aws_security_group" "instance" {
     to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = "${var.ssh_port}"
+    to_port = "${var.ssh_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "terraform-example-sg"
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   default = 8080
 }
+variable "ssh_port" {
+  description = "The port the server will use for SSH Connection"
+  default = 22
+}
